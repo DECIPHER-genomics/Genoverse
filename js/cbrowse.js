0d8a8548013f0294ae25c1978804b4e6a2991741
Simon Brent <simon.brent@gmail.com>
2012-01-16 14:42:11 +0000

    Removed calls and feature info functionality. Added bump array for use by tracks. Added hasData array [ start, stop ] - used to decide when to get new data from server. Added this.length = this.stop - this.start Changed replot to happen on window location change event, removing the need for setTimeouts

--- js/cbrowse.js
+++ js/cbrowse.js
@@ -12,6 +12,7 @@ var CBrowse = Base.extend({
     zoom: 1,
     start: 0,
     delta: 0,
+    hasData: [],
     colors: {
       foreground: '#000000',
       border: '#A3A3A3',
@@ -19,11 +20,23 @@ var CBrowse = Base.extend({
     }
   },
 
-  constructor: function (config) {  
+  constructor: function (config) {
     $.extend(this, this.defaults, config);
     
     if (!(this.container && this.container.length)) {
       this.die('You must supply a ' + (this.container ? 'valid ' : '') + 'container element');
+    }
+    
+    this.bump = [];
+    
+    var i = this.width * 3;
+    
+    while (i--) {
+      this.bump.push(0);
+    }
+    
+    if (this.start && this.stop) {
+      this.setRange(this.start, this.stop);
     }
     
     this.initTracks();
@@ -43,7 +56,7 @@ var CBrowse = Base.extend({
     };
     
     for (var i = 0; i < this.tracks.length; i++) {
-      //Copy some default values from cBrowse to Track
+      // Copy some default values from cBrowse to Track
       this.tracks[i] = new CBrowse.Track[this.tracks[i].type]($.extend(defaults, { offsetY: this.height, i: i }, this.tracks[i]));
       this.height   += this.tracks[i].height;
     }
@@ -58,8 +71,10 @@ var CBrowse = Base.extend({
   },
   
   setRange: function (start, stop) {
-    this.start = parseInt(start, 10);
-    this.stop  = parseInt(stop,  10);
+    this.prevStart = this.start;
+    this.prevStop  = this.stop;
+    this.start     = parseInt(start, 10);
+    this.stop      = parseInt(stop,  10);
     
     // THIS SHOULD NEVER HAPPEN
     if (this.stop < this.start) {
@@ -67,8 +82,14 @@ var CBrowse = Base.extend({
       this.start = parseInt(stop, 10);
     }
     
-    this.zoom = this.chromosome.size / (this.stop - this.start);
+    this.length = (this.stop - this.start) || 1; // TODO: check when start = stop
+    this.zoom   = this.chromosome.size / this.length;
+    
     this.initScale();
+    
+    if (this.prevStart !== this.start || this.prevStop !== this.stop) {
+      this.updateURL();
+    }
   },
   
   setTracks: function (tracks) {
@@ -80,16 +101,21 @@ var CBrowse = Base.extend({
     var cBrowse = this;
     
     $.when.apply($, $.map(this.tracks, function (track) { return track.getDataAndPlot(); })).done(function () {
-      cBrowse.updateImage();
-      cBrowse.updateCallShortCuts();
+      cBrowse.hasData = [ cBrowse.start - cBrowse.length, cBrowse.stop + cBrowse.length ];
+      cBrowse.plot();
+      
+      /*if (cBrowse.GO) {
+        cBrowse.dragging = true;
+        cBrowse.GO       = false;
+      }*/
     });
   },
   
   initDOM: function () {
-    this.viewPort    = $('.viewport',     this.container).css({ width: this.width, height: this.height });
     this.canvas      = $('canvas',        this.container).attr('width', 3 * this.width).attr('height', this.height).css('left', -this.width);
-    this.mask        = $('.mask',         this.container).css({ width: this.width, height: this.height, top: this.viewPort.offset().top }).show();
+    this.mask        = $('.mask',         this.container).show();
     this.featureInfo = $('.feature_info', this.container);
+    /*this.viewPort    = */$('.viewport',     this.container).css({ width: this.width, height: this.height });
     this.context     = this.canvas[0].getContext('2d');    
     this.offset      = this.canvas.offset();
     
@@ -108,33 +134,23 @@ var CBrowse = Base.extend({
   },
   
   zoomIn: function (x) {
-    var cBrowse = this;
-    
     if (!x) {
       x = this.width / 2;
     }
     
-    this.mask.show();
-    
-    var start = this.start + (x - 2 * this.delta) / (2 * this.scale);
-    var stop  = start + (this.stop - this.start) / 2;
+    var start = this.start + x / (2 * this.scale);
+    var stop  = start + this.length / 2;
 
     this.setRange(start, stop);
-    
-    setTimeout(function () { cBrowse.plot(); }, 100);
   },
   
   zoomOut: function (x) {
-    var cBrowse = this;
-    
     if (!x) {
       x = this.width / 2;
     }
     
-    this.mask.show();
-    
-    var start = this.start - (x + this.delta) / this.scale;
-    var stop  = start + 2 * (this.stop - this.start);
+    var start = this.start - x / this.scale;
+    var stop  = start + 2 * this.length;
 
     if (start < 0) {
       start = 0;
@@ -145,24 +161,31 @@ var CBrowse = Base.extend({
     }
 
     this.setRange(start, stop);
-    
-    setTimeout(function () { cBrowse.plot(); }, 100);
+  },
+  
+  updateURL: function () {
+    window.history.pushState({}, "", this.baseURL + '?r=' + this.chromosome.id + ':' + this.start + '-' + this.stop);
+    window.location.hash = '';
+    //window.location.hash = 'r=' + this.chromosome.id + ':' + this.start + '-' + this.stop;
   },
   
   initEventHandlers: function () {
     var cBrowse = this;
     
-    $('a.zoom_in').click(function () {
+    $('a.zoom_in', this.container).click(function () {
       cBrowse.zoomIn();
+    //  return false;
     });
     
-    $('a.zoom_out').click(function () {
+    $('a.zoom_out', this.container).click(function () {
       cBrowse.zoomOut();
+     // return false;
     });
     
     this.canvas.dblclick(function (e) {
       var x = e.pageX - cBrowse.offset.left - cBrowse.width;
       cBrowse.zoomIn(x);
+      return false;
     });
     
     this.canvas.mousedown(function (e) {
@@ -193,63 +216,50 @@ var CBrowse = Base.extend({
       }
     });
     
+    function mouseup(e) {
+      if (cBrowse.dragging) {
+        var delta = e.pageX - cBrowse.draggingOffsetX - cBrowse.delta;
+        var start = cBrowse.start - delta / cBrowse.scale;
+        
+        cBrowse.delta += delta;
+        
+        console.log('delta: ' + cBrowse.delta);
+        
+        cBrowse.setRange(start, start + cBrowse.length);
+        cBrowse.dragging = false;
+      }
+    }
+    
     $(document).mousemove(function (e) {
       if (cBrowse.dragging) {
         var x = e.pageX - cBrowse.draggingOffsetX;
         
         cBrowse.lastClientX = e.pageX;
-        cBrowse.hideFeatureInfo();
         cBrowse.offsetImage(x);
+        
+        // FIXME: after first reload, reload happens all the time if drag is maintained, because x is always outside the limit. find way to change limit
+        /*if (Math.abs(x) > cBrowse.width) {
+          cBrowse.GO = true;
+          mouseup(e);
+        }*/
       }
     });
     
     $(document).mouseup(function (e) {
       console.log('mouseup');
-      
-      if (cBrowse.dragging) {
-        cBrowse.dragging = false;
-        cBrowse.delta    = e.pageX - cBrowse.draggingOffsetX;
-
-        console.log('delta: ' + cBrowse.delta);
-        
-        if (Math.abs(cBrowse.delta) < cBrowse.width) {
-          cBrowse.updateURL(cBrowse.delta);
-          return false;
-        }
-        
-        cBrowse.mask.show();
-        
-        cBrowse.start = cBrowse.start - cBrowse.delta/cBrowse.scale;
-        
-        if (cBrowse.delta > 0) {
-          // moved right
-          setTimeout(function () { cBrowse.plot(0, cBrowse.delta); }, 100);
-          cBrowse.delta = 0;
-        } else { 
-          // moved left
-          setTimeout(function () { cBrowse.plot(3*cBrowse.width + cBrowse.delta, 3*cBrowse.width); }, 100);
-          cBrowse.delta = 0;
-        }
-      }
+      mouseup(e);
     });
-  },
-  
-  hideFeatureInfo: function () {
-    this.featureInfo.hide();
-  },
-  
-  updateURL: function (delta) {
-    var start, stop;
     
-    if (delta) {
-      start = this.start - delta / this.scale;
-    } else {
-      start = this.start;
+    window.onpopstate = function () { cBrowse.PLOT2(); };
+  },
+  
+  PLOT2: function () {
+    if (this.start - this.hasData[0] >= 0 && this.hasData[1] - this.stop >= 0) {
+      return this.dragging && Math.abs(this.delta) < this.width ? false : this.plot();
     }
     
-    stop = start + this.chromosome.size / this.zoom;
-    
-    window.history.pushState({}, "", this.baseURL + start + ";" + stop);
+    this.mask.show();
+    this.getDataAndPlot();
   },
   
   updateImage: function (x1, x2) {
@@ -258,70 +268,22 @@ var CBrowse = Base.extend({
       x2 = 3 * this.width;
     }
     
+    //save canvas image as data url (png format by default)
     this.dataURL   = this.canvas[0].toDataURL();
     this.image.src = this.dataURL;
     
     this.mask.hide();
   },
   
-  updateCallShortCuts: function () {
-    var cBrowse = this;
-    var select = $('#calls');
-    var track, j, call;
-    
-    for (var i = 0; i < this.tracks.length; i++) {
-      track = this.tracks[i];
-      
-      if (track.calls && track.calls.length) {
-        for (j = 0; j < track.calls.length; j++) {
-          call = track.calls[j];
-          
-          select.append($('<option>', call).text(
-            "Source: " + track.name +
-            "; Size "  + (call.stop - call.start) +
-            "; Ratio " + call.ratio +
-            "; "       + this.chromosome.id + ":" +call.start.toLocaleString() + "-" + call.stop.toLocaleString()
-          ));
-        }
-        
-        select.change(function () {
-          var option = $('option:selected', this);
-          
-          if (!option.attr('start')) {
-            return false;
-          }
-          
-          cBrowse.mask.show();
-          
-          var start = parseInt(option.attr('start'), 10);
-          var stop  = parseInt(option.attr('stop'),  10);
-          var size  = stop - start;
-          var k     = size < 100000 ? 20 : size < 1000000 ? 5 : 1;
-
-          start = start - k * size;
-          stop  = stop  + k * size;
-          
-          cBrowse.setRange(start, stop);
-          
-          setTimeout(function () { cBrowse.plot(); }, 100);
-        });
-        
-        select.removeAttr('disabled');
-      }
-    }
-  },
-  
   plot: function (x1, x2) {
     console.time("plot");
-    
-    this.hideFeatureInfo();
     
     if (!x1 && !x2) {
       x1 = 0;
       x2 = 3 * this.width;
     }
     
-    //TODO: reset dragging offsets into separate routine
+    // TODO: reset dragging offsets into separate routine
     this.delta             = 0;
     this.context.fillStyle = this.colors.foreground;
     this.offsetX           = this.start * this.scale;
@@ -332,15 +294,9 @@ var CBrowse = Base.extend({
       this.tracks[i].plot(x1, x2);
     }  
     
-    //save canvas image as data url (png format by default)
-    this.dataURL   = this.canvas[0].toDataURL();
-    this.image.src = this.dataURL;
-    
-    this.mask.hide();
+    this.updateImage();
 
     console.timeEnd("plot");
-    
-    this.updateURL();
   },
   
   offsetImage: function (x) {
