0ccef02b439dffbfb891bc47a8e94bf3c7ab2a27
Simon Brent <simon.brent@gmail.com>
2012-02-27 09:12:37 +0000

    Make Scalebar a child of Block

--- js/Scalebar.js
+++ js/Scalebar.js
@@ -1,37 +1,36 @@
-CBrowse.Track.Scalebar = CBrowse.Track.extend({
+CBrowse.Track.Scalebar = CBrowse.Track.Block.extend({
   defaults: {
     height        : 20,
     featureHeight : 3,
     labelY        : 15,
-    borderTop     : 0.5,
-    borderBottom  : 3.5,
-    guideLines    : true
-  },
-  
-  reset: function () {
-    this.base();
-    this.features = {};
+    color         : '#000000',
+    guideLines    : true,
+    bump          : false,
+    forceLabels   : true,
+    labelUnits    : [ 'bp', 'Kb', 'Mb', 'Gb', 'Tb' ]
   },
   
   setScale: function () {
+    this.base();
+    
     var length = this.cBrowse.length;
     var majorUnit, minorUnit, exponent, mantissa;
     
     if (length <= 51) {
       majorUnit = 10;
-      minorUnit = 1; 
+      minorUnit = 1;
     } else {
       exponent = Math.pow(10, Math.floor(Math.log(length) / Math.log(10)));
       mantissa = length / exponent;
       
       if (mantissa < 1.2) {
-        majorUnit = exponent / 10;
+        majorUnit = exponent  / 10;
         minorUnit = majorUnit / 5;
       } else if (mantissa < 2.5) {
-        majorUnit = exponent / 5;
+        majorUnit = exponent  / 5;
         minorUnit = majorUnit / 4;
       } else if (mantissa < 5) {
-        majorUnit = exponent / 2;
+        majorUnit = exponent  / 2;
         minorUnit = majorUnit / 5;
       } else {
         majorUnit = exponent;
@@ -41,108 +40,97 @@ CBrowse.Track.Scalebar = CBrowse.Track.extend({
     
     this.minorUnit = this.cBrowse.minorUnit = minorUnit;
     this.majorUnit = this.cBrowse.majorUnit = majorUnit;
+    this.seen      = {};
+    this.features  = new RTree();
     
     if (this.guideLines) {
       this.cBrowse.guideLines = { major: {}, minor: {} };
     }
-    
-    this.features = {};
-    
-    this.base();
   },
   
-  positionData: function (start, end) {
-    var flip  = 1;
-    var width = Math.round(this.minorUnit * this.scale);
-    var features, start, major;
+  setFeatures: function (start, end) {
+    start  = Math.max(start - (start % this.minorUnit), 0);
     
-    this.labelOffset = Math.ceil(this.fontWidth / this.scale) * this.cBrowse.longestLabel;
+    var flip = (start / this.minorUnit) % 2 ? 1 : -1;
+    var data = [];
+    var feature, major;
     
-    for (var x = start - (start % this.minorUnit); x < end - (end % this.minorUnit) + this.minorUnit; x += this.minorUnit) {
-      if (this.features[x]) {
+    for (var x = start; x < end + this.minorUnit; x += this.minorUnit) {
+      flip *= -1;
+      
+      if (this.seen[x]) {
         continue;
       }
       
-      features = [];
-      start    = Math.round(x * this.scale);
-      major    = x && !(x % this.majorUnit);
+      this.seen[x] = 1;
       
-      if ((flip *= -1) === 1) {
-        features.push([ 'fillRect', [ start, 0, width, this.featureHeight ] ]); // Black part of scalebar
+      feature = { id: x };
+      major   = x && !(x % this.majorUnit);
+      
+      if (flip === 1) {
+        feature.start  = x;
+        feature.end    = x + this.minorUnit;
+        feature.colour = this.color;
       }
       
       if (major) {
-        features.push([ 'fillText', [ x.toLocaleString(), start, this.labelY ] ]); // Location label
-        features.push([ 'fillRect', [ start, this.borderBottom, 1, 3 ] ]);         // Indicator dash
+        feature.label = this.formatLabel(x);
+        
+        if (!feature.end) {
+          feature.start  = x;
+          feature.end    = x;
+          feature.colour = this.color;
+        }
       }
       
-      if (features.length) {
-        this.features[x] = features;
+      if (feature.end) {
+        data.push(feature);
       }
       
-      if (this.guideLines) {
-        this.cBrowse.guideLines[major ? 'major' : 'minor'][x] = start;
+      if (this.guideLines !== false) {
+        this.cBrowse.guideLines[major ? 'major' : 'minor'][x] = Math.round(x * this.scale);
       }
     }
+    
+    this.base(data);
+  },
+  
+  positionData: function (data, edges, func) {
+    var data = this.base(data, edges, func);
+    this.data = data[this.color];
+    return data;
   },
   
   makeImage: function (start, end, width, moved) {
-    var func     = moved < 0 ? 'unshift' : 'push';
-    var pos      = moved < 0 ? 'right'   : 'left';
-    var div      = this.imgContainer.clone().width(width);
-    var deferred = $.Deferred();
-    
-    this.positionData(start, end);
-    this.draw(start, end, width);
-    
-    this.imgContainers[func](div.append($('<img />').load(deferred.resolve).attr('src', this.canvas[0].toDataURL())).css(pos, this.offsets[pos])[0]);
-    this.container.append(this.imgContainers);
-    
-    this.offsets[pos] += width;
-    
-    return deferred;
+    this.setFeatures(start, end);
+    return this.base(start, end, width, moved);
   },
   
-  draw: function (start, end, width) {
-    this.canvas.attr({ width: width, height: this.fullHeight });
+  drawCallback: function (image) {
+    var i = this.data.length;
     
-    this.context.fillStyle = this.cBrowse.colors.background;
-    this.context.fillRect(0, 0, width, this.fullHeight);
-    
-    var scaledStart = Math.round(start * this.scale) + 1;
-    var i, position;
-    
-    if (this.guideLines) {
-      this.cBrowse.decorateTrack(start, end, this);
-    }
-    
-    start -= this.labelOffset; // Ensure drawing of labels which overlap the edge of the image
-    
-    this.context.fillStyle = '#000';
-    
-    for (var x = start - (start % this.minorUnit); x < end; x += this.minorUnit) {
-      i = this.features[x] ? this.features[x].length : 0;
-      
-      while (i--) {
-        position = this.features[x][i][1].slice();
-        position[this.features[x][i][0] === 'fillText' ? 1 : 0] -= scaledStart;
-        
-        this.context[this.features[x][i][0]].apply(this.context, position);
+    while (i--) {
+      if (this.data[i][0] === 'fillText') {
+        this.context.fillRect(Math.round(this.data[i][1][1]), this.featureHeight, 1, 3);
       }
     }
     
-    this.hLine(this.borderTop,    0, width);
-    this.hLine(this.borderBottom, 0, width);
+    this.context.fillRect(0, 0,                  image.width, 1);
+    this.context.fillRect(0, this.featureHeight, image.width, 1);
   },
   
-  drawLine: function (x1, y1, x2, y2) {
-    this.context.beginPath();
-    this.context.moveTo(x1, y1);
-    this.context.lineTo(x2, y2);
-    this.context.stroke();
-  },
-  
-  hLine: function (y, x1, x2) {
-    this.drawLine(x1, y, x2, y);
+  formatLabel: function (x) {
+    var str = x.toString();
+    
+    if (this.minorUnit < 1000) {
+      return str.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+    } else {
+      var power = Math.floor((str.length - 1) / 3);
+      var unit  = this.labelUnits[power];
+      
+      x /= Math.pow(10, power * 3);
+      
+      return Math.floor(x) + (unit === 'bp' ? '' : '.' + (x.toString().split('.')[1] || '').concat('00').substring(0, 2)) + ' ' + unit;
+    }
   }
 });
