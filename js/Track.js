d99142d698c38c70dd7758986143f085a359c581
Simon Brent <simon.brent@gmail.com>
2012-04-03 14:02:35 +0100

    added init added ajax aborting added drawBackground and drawBackgroundColor added support for labels to be drawn on a separate image, below features don't add bounds for overlaid labels added support for bumping labels, but not bumping features fixed rounding issues with borders fixed history when returning to a previous zoom level after having scrolled

--- js/Track.js
+++ js/Track.js
@@ -1,30 +1,32 @@
 CBrowse.Track = Base.extend({
   defaults: {
-    height : 10,
-    bump   : false
+    height      : 10,
+    bump        : false,
+    bumpSpacing : 2,
+    urlParams   : {}
   },
   
   constructor: function (config) {
     var track = this;
     
-    $.extend(this, this.defaults, config);
+    $.extend(this, this.defaults, this.config, config);
     
-    this.spacing       = typeof this.spacing === 'undefined' ? this.cBrowse.trackSpacing : this.spacing;
-    this.featureHeight = this.featureHeight || this.height;
-    this.fixedHeight   = typeof this.fixedHeight === 'undefined' ? this.featureHeight === this.height : this.fixedHeight;
-    this.height       += this.spacing;
-    this.canvas        = $('<canvas>').appendTo(this.canvasContainer);
-    this.container     = $('<div class="track_container">').height(this.height).appendTo(this.canvasContainer),
-    this.imgContainer  = $('<div class="image_container">');
-    this.context       = this.canvas[0].getContext('2d');
-    this.fontHeight    = parseInt(this.context.font, 10);
-    this.fontWidth     = this.context.measureText('W').width;
-    this.fullHeight    = this.height;
-    this.maxHeight     = this.height;
-    this.initialHeight = this.height;
-    this.scaleSettings = {};
-    this.features      = new RTree();
+    this.spacing           = typeof this.spacing === 'undefined' ? this.cBrowse.trackSpacing : this.spacing;
+    this.featureHeight     = this.featureHeight || this.height;
+    this.fixedHeight       = typeof this.fixedHeight === 'undefined' ? this.featureHeight === this.height : this.fixedHeight;
+    this.height           += this.spacing;
+    this.canvas            = $('<canvas>').appendTo(this.canvasContainer);
+    this.container         = $('<div class="track_container">').height(this.height).appendTo(this.canvasContainer),
+    this.imgContainer      = $('<div class="image_container">');
+    this.context           = this.canvas[0].getContext('2d');
+    this.fontHeight        = parseInt(this.context.font, 10);
+    this.fontWidth         = this.context.measureText('W').width;
+    this.separateLabels    = this.separateLabels || !!this.depth;
+    this.initialHeight     = this.height;
+    this.maxHeight         = this.height;
+    this.maxFeaturesHeight = 0;
     
+    this.init();
     this.setScale();
     
     if (this.name) {
@@ -40,7 +42,7 @@ CBrowse.Track = Base.extend({
         var height;
         
         switch (e.target.className) {
-          case 'expand'   : height = $(this).data('height'); track.autoHeight = true; break;
+          case 'expand'   : height = $(this).data('height'); track.autoHeight = true;  break;
           case 'collapse' : height = track.initialHeight;    track.autoHeight = false; break;
           default         : return;
         }
@@ -55,8 +57,8 @@ CBrowse.Track = Base.extend({
       }
       
       var x        = e.pageX - track.container.parent().offset().left + track.cBrowse.scaledStart;
-      var y        = e.pageY - track.container.offset().top;
-      var features = track.featurePositions.search({ x: x, y: y, w: 1, h: 1 });
+      var y        = e.pageY - $(e.target).offset().top;
+      var features = track[e.target.className === 'labels' ? 'labelPositions' : 'featurePositions'].search({ x: x, y: y, w: 1, h: 1 });
       var i        = features.length;
       var seen     = {};
       
@@ -72,10 +74,17 @@ CBrowse.Track = Base.extend({
     });
   },
   
-  reset: function () {
+  init: function () {
     this.scaleSettings = {};
     this.features      = new RTree();
+  },
+  
+  reset: function () {
+    if (this.ajax) {
+      this.ajax.abort();
+    }
     
+    this.init();
     this.container.empty();
   },
   
@@ -125,7 +134,7 @@ CBrowse.Track = Base.extend({
     this.scale = this.cBrowse.scale;
     
     if (this.scaleSettings[this.scale] && !this.cBrowse.history[this.cBrowse.start + ':' + this.cBrowse.end]) {
-      this.container.children('.scale_' + this.scale.toString().replace('.', '_')).remove();
+      this.container.children('.' + this.cBrowse.scrollStart).remove();
       delete this.scaleSettings[this.scale];
     }
     
@@ -136,13 +145,17 @@ CBrowse.Track = Base.extend({
         imgContainers    : [],
         overlaps         : []
       };
+      
+      this.scaleSettings[this.scale].labelPositions = this.separateLabels ? new RTree() : this.scaleSettings[this.scale].featurePositions;
     }
     
     var scaleSettings = this.scaleSettings[this.scale];
     
-    $.each([ 'offsets', 'featurePositions', 'imgContainers', 'overlaps' ], function () {
+    $.each([ 'offsets', 'featurePositions', 'labelPositions', 'imgContainers', 'overlaps' ], function () {
       track[this] = scaleSettings[this];
     });
+    
+    this.maxFeaturesHeight = 0;
     
     this.container.css('left', 0).children().hide();
   },
@@ -156,12 +169,13 @@ CBrowse.Track = Base.extend({
   },
   
   positionData: function (data, edges, func) {
-    var feature, start, end, x, y, width, bounds, bump, j, k, noLabel;
-    var maxIndex = this.forceLabels || (data.length && data[0].label && this.cBrowse.length < 1e7) ? 1 : 0;
-    var height   = this.initialHeight;
-    var scale    = this.scale > 1 ? this.scale : 1;
-    var seen     = {};
-    var features = { fill: {}, border: {}, label: {} };
+    var feature, start, end, x, y, width, bounds, bump, depth, j, k, labelWidth, maxIndex;
+    var showLabels   = this.forceLabels === true || !(this.maxLabelRegion && this.cBrowse.length > this.maxLabelRegion);
+    var height       = this.initialHeight;
+    var labelsHeight = 1;
+    var scale        = this.scale > 1 ? this.scale : 1;
+    var seen         = {};
+    var features     = { fill: {}, border: {}, label: {} };
     
     this.colorOrder = [];
     
@@ -174,45 +188,92 @@ CBrowse.Track = Base.extend({
       
       seen[feature.id] = 1;
       
-      start   = feature.scaledStart - edges.start;
-      end     = feature.scaledEnd   - edges.start;
-      bounds  = feature.bounds;
-      width   = start > end ? 1 : (end - start) || scale;
-      noLabel = !feature.label || (scale > 1 && start < 0);
+      start      = feature.scaledStart - edges.start;
+      end        = feature.scaledEnd   - edges.start;
+      bounds     = feature.bounds;
+      labelWidth = feature.label ? Math.ceil(this.context.measureText(feature.label).width) + 1 : 0;
       
-      if (!bounds) {
-        x      = feature.scaledStart;
-        y      = feature.y || 0;
-        bounds = [{ x: x, y: y, w: width, h: this.featureHeight + maxIndex + 1 }];
+      if (bounds) {
+        width      = bounds[0].w   - 1;
+        maxIndex   = bounds.length - 1;
+      } else {
+        width = end - start;
         
-        if (maxIndex) {
-          bounds.push({ x: x, y: y + bounds[0].h, w: Math.ceil(this.context.measureText(feature.label).width) + 1, h: this.fontHeight + 2 });
+        if (width < 0) {
+          width = 1;
+        } else if (width < 1) {
+          width = scale;
         }
         
+        x      = feature.scaledStart;
+        y      = feature.y || 0;
+        bounds = [{ x: x, y: y, w: width + 1, h: this.featureHeight + this.bumpSpacing }];
+        
+        if (feature.label && showLabels && !this.labelOverlay && this.forceLabels !== 'off' && !(scale > 1 && start < 0)) {
+          if (this.separateLabels) {
+            bounds.push({ x: x, y: y, w: labelWidth, h: this.fontHeight + 2 });
+          } else {
+            bounds.push({ x: x, y: y + this.featureHeight + 1, w: Math.max(labelWidth, width + 1), h: this.fontHeight + 2 });
+          }
+        }
+        
+        maxIndex = bounds.length - 1;
+        
+        bounds[0].h += this.separateLabels ? 0 : maxIndex;
+        
         if (this.bump) {
-          do {
-            bump = false;
-            j    = bounds.length;
+          depth = 0;
+          
+          if (this.separateLabels) { // labels are drawn on a separate image, below the features
+            j = bounds.length;
             
             while (j--) {
-              if ((this.featurePositions.search(bounds[j])[0] || feature).id !== feature.id) {
-                k = bounds.length;
-                
-                while (k--) {
-                  bounds[k].y += bounds[j].h;
+              do {
+                if (j === 0 && this.depth && ++depth >= this.depth) {
+                  break;
                 }
                 
-                bump = true;
+                bump = false;
+                
+                if ((this[j ? 'labelPositions' : 'featurePositions'].search(bounds[j])[0] || feature).id !== feature.id) {
+                  bounds[j].y += bounds[j].h;
+                  bump      = true;
+                }
+              } while (bump);
+            }
+          } else { // labels and features drawn on the same image
+            do {
+              bump = false;
+              j    = bounds.length;
+              
+              while (j--) {
+                if ((this[j ? 'labelPositions' : 'featurePositions'].search(bounds[j])[0] || feature).id !== feature.id) {
+                  k = bounds.length;
+                  
+                  while (k--) {
+                    bounds[k].y += bounds[j].h; // bump both feature and label by the height of the current bounds
+                  }
+                  
+                  bump = true;
+                }
               }
+            } while (bump);
+          }
+        } else if (this.bumpLabels && bounds[1]) { // labels are bumped, but features aren't
+          do {
+            bump = false;
+            
+            if ((this.labelPositions.search(bounds[1])[0] || feature).id !== feature.id) {
+              bounds[1].y++;
+              bump = true;
             }
           } while (bump);
         }
         
         this.featurePositions.insert(bounds[0], feature);
         
-        if (maxIndex) {
-          bounds[1].h += 2;
-          this.featurePositions.insert(bounds[1], feature);
+        if (bounds[1]) {
+          this.labelPositions.insert(bounds[1], feature);
         }
       }
       
@@ -228,8 +289,10 @@ CBrowse.Track = Base.extend({
         features.border[feature.borderColor] = [];
       }
       
-      if (feature.labelOverlay && !features.label[feature.labelColor || feature.color]) {
-        features.label[feature.labelColor || feature.color] = [];
+      if ((this.separateLabels || this.labelOverlay) && !features.label[feature.labelColor]) {
+        features.label[feature.labelColor] = [];
+      } else if (feature.labelColor && feature.labelColor !== feature.color && !features.fill[feature.labelColor]) {
+        features.fill[feature.labelColor] = [];
       }
       
       if (scale > 1 && start < end) {
@@ -241,52 +304,72 @@ CBrowse.Track = Base.extend({
       features.fill[feature.color].push([ 'fillRect', [ start, bounds[0].y, width, this.featureHeight ] ]);
       
       if (feature.borderColor) {
-        features.border[feature.borderColor].push([ 'strokeRect', [ Math.round(start), Math.round(bounds[0].y) + 0.5, Math.round(width), Math.round(this.featureHeight) ] ]);
+        features.border[feature.borderColor].push([ 'strokeRect', [ start, bounds[0].y + 0.5, width, this.featureHeight ] ]);
       }
       
-      if (maxIndex && !noLabel) {
-        if (!feature.labelOverlay) {
-          features.fill[feature.color].push([ 'fillText', [ feature.label, start, bounds[1].y ], feature.labelColor ]);
-        } else if (bounds[1].w < bounds[0].w) {
-          features.label[feature.labelColor || feature.color].push([ 'fillText', [ feature.label, start + (feature.textAlign === 'center' ? (bounds[0].w - bounds[1].w) / 2 : 0), bounds[0].h / 2 ] ]);
-        }
+      if (this.labelOverlay && labelWidth < width - 1) { // Don't show overlaid labels on features which aren't wider than the label
+        features.label[feature.labelColor].push([ 'fillText', [ feature.label, start + (width - labelWidth) / 2, bounds[0].y + bounds[0].h / 2 ], feature.labelColor ]);
+      } else if (bounds[1]) {
+        features[this.separateLabels ? 'label' : 'fill'][feature.labelColor].push([ 'fillText', [ feature.label, start, bounds[1].y ], feature.labelColor ]);
       }
       
-      if ((feature.scaledStart + Math.max(width, maxIndex ? bounds[1].w : 0) > edges.end) || (feature.scaledStart < edges.start)) {
+      if ((feature.scaledStart + Math.max(width, bounds[1] ? labelWidth : 0) > edges.end) || (feature.scaledStart < edges.start)) {
         this.overlaps[func]($.extend({}, feature, { bounds: bounds }));
       }
       
-      feature.bottom = bounds[maxIndex].y + bounds[maxIndex].h;
+      if (this.separateLabels && bounds[1]) {
+        feature.bottom      = bounds[0].y + bounds[0].h + this.spacing;
+        feature.labelBottom = bounds[1].y + bounds[1].h + this.spacing;
+        labelsHeight        = Math.max(feature.labelBottom, labelsHeight);
+      } else {
+        feature.bottom = bounds[maxIndex].y + bounds[maxIndex].h + this.spacing;
+      }
       
       height = Math.max(feature.bottom, height);
     }
     
-    this.fullHeight = height;
-    this.maxHeight  = Math.max(height, this.maxHeight);
+    this.featuresHeight    = height;
+    this.labelsHeight      = labelsHeight;
+    this.fullHeight        = height + labelsHeight;
+    this.maxHeight         = Math.max(this.fullHeight, this.maxHeight);
+    this.maxFeaturesHeight = Math.max(height, this.maxFeaturesHeight);
     
     return features;
   },
   
-  beforeDraw: function (image) {
-    var colors      = { major: [ this.cBrowse.colors.majorScaleLine, this.cBrowse.majorUnit ], minor: [ this.cBrowse.colors.minorScaleLine, this.cBrowse.minorUnit ] };
+  drawBackground: function (image, height) {
+    var scaleLines  = { major: [ this.cBrowse.colors.majorScaleLine, this.cBrowse.majorUnit ], minor: [ this.cBrowse.colors.minorScaleLine, this.cBrowse.minorUnit ] };
     var scaledStart = Math.round(image.start * this.scale) + 1;
-    var x, c;
+    var x;
     
-    for (c in colors) {
-      this.context.fillStyle = colors[c][0];
-      
-      for (x = Math.max(image.start - (image.start % colors[c][1]), 0); x < image.end + this.cBrowse.minorUnit; x += colors[c][1]) {
-        this.context.fillRect((this.cBrowse.scaleLines[c][x] || 0) - scaledStart, 0, 1, this.fullHeight);
-      }
+    if (this.cBrowse.backgrounds) {
+      this.drawBackgroundColor(scaledStart, height);
     }
     
-    if (this.borderColor) {
-      this.context.fillStyle = this.borderColor;
+    for (var c in scaleLines) {
+      this.context.fillStyle = scaleLines[c][0];
       
-      this.context.fillRect(0, 0,                      image.width, 1);
-      this.context.fillRect(0, this.featureHeight - 1, image.width, 1);
+      for (x = Math.max(image.start - (image.start % scaleLines[c][1]), 0); x < image.end + this.cBrowse.minorUnit; x += scaleLines[c][1]) {
+        this.context.fillRect((this.cBrowse.scaleLines[c][x] || 0) - scaledStart, 0, 1, height);
+      }
     }
   },
   
-  afterDraw: $.noop  // implement in children
+  drawBackgroundColor: function (scaledStart, height) {
+    var backgrounds = this.cBrowse.backgrounds;
+    var i;
+    
+    for (var c in backgrounds) {
+      this.context.fillStyle = c;
+      
+      i = backgrounds[c].length;
+      
+      while (i--) {
+        this.context.fillRect(backgrounds[c][i].scaledStart - scaledStart, 0, backgrounds[c][i].scaledEnd - backgrounds[c][i].scaledStart, height);
+      }
+    }
+  },
+  
+  beforeDraw : $.noop,  // implement in children
+  afterDraw  : $.noop   // implement in children
 });
