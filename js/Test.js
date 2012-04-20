c1dd42afb299ddbf8760968e6dfcc3af268aa6ee
Eugene Bragin <eugene.bragin@gmail.com>
2012-04-20 18:08:29 +0100

    sorting RTree madness (not done yet)

--- js/Test.js
+++ js/Test.js
@@ -1,8 +1,8 @@
 CBrowse.Track.Test = CBrowse.Track.extend({
   config: {
-    height        : 200,
+    height        : 150,
     fixedHeight   : true,
-    featureHeight : 1,
+    featureHeight : 2,
     color         : '#000000',
     bump          : false,
     autoHeight    : false,
@@ -10,28 +10,49 @@ CBrowse.Track.Test = CBrowse.Track.extend({
   },
 
   setFeatures: function (json) {
+    console.time("timing setFeatures");
     var i = json.data.length;
-    
+    debugger;
     while (i--) {
       var feature = {
         sort: i,
+        start: json.data[i][0],
+        end:  json.data[i][0] + 10,
+        id: "f" + i,
+        color: "#000000",
+        y: this.height/2 - json.data[i][1]*this.height/4,
+        decorations: [], 
         bounds: {},
         visible: {},
         bottom: {},
         labelBottom: {}
       };
 
-      this.features.insert({ x: json.data[i][0], y: 10, w: 10, h: 1 }, feature);
+      //this.features.insert({ x: json.data[i][0], y: 0, w: 10, h: 1 }, feature);
+      json.data[i] = feature;
     }
-    
+
+    json.features = json.data;
+    delete json.data;
+   
     if (this.allData) {
       this.url = false;
     }
+
+    console.timeEnd("timing setFeatures");
+  },
+
+  beforeDraw: function (image) {
+    this.context.fillStyle = '#89BDFF';
+    this.context.fillRect(0, this.height/2, image.width, 1);
+    this.context.fillRect(0, this.height/4, image.width, 1);
+    this.context.fillRect(0, 3*this.height/4, image.width, 1);
   },
 
   afterDraw: function (image) {
-    this.context.fillStyle = 'red';
+    this.context.fillStyle = 'black';
     this.context.fillRect(0, 0, image.width, 1);
+    this.context.fillRect(0, this.height-1, image.width, 1);
   }
 
 });
