ec7b2e19c2c3f882e90865ff020d1dd718d75674
Eugene Bragin <eugene.bragin@gmail.com>
2012-05-24 16:20:50 +0100

    changed Overlap plugin to hide overlap on different event (beforeMove)

--- js/plugins/overlap.js
+++ js/plugins/overlap.js
@@ -1,7 +1,7 @@
 // Default css for overlap bar
 // Reason it's here is so that developer only has to include this js file for entire plugin functionality
 // Any additional custom css for .overlap will overwrite it
-document.styleSheets[0].insertRule(".overlap{ background: none repeat scroll 0 0 #000000; display: none; height: 900px; opacity: 0.08; position: absolute; top: 0; z-index: 10; }", 0);
+document.styleSheets[0].insertRule(".overlap{ background: none repeat scroll 0 0 #000000; display: none; height: 900px; opacity: 0.08; position: absolute; top: 0; z-index: 30; }", 0);
 
 CBrowse.Track.on('afterMakeMenu', function (feature, e) {
   if (!this.overlap) return;
@@ -15,6 +15,6 @@ CBrowse.Track.on('afterMakeMenu', function (feature, e) {
     .show();
 });
 
-CBrowse.on('beforeMousedown', function () {
+CBrowse.on('beforeMove', function () {
   $('div.overlap').remove();  
 });
