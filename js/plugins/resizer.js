0ef15cf1d1e23af6a2fd182853b424a3a1c292ea
Simon Brent <simon.brent@gmail.com>
2012-05-31 14:15:15 +0100

    Fixing styles on resizer and expander

--- js/plugins/resizer.js
+++ js/plugins/resizer.js
@@ -1,9 +1,11 @@
 // Default css
 // Reason it's here is so that developer only has to include this js file for entire plugin functionality
 // Any additional custom css will overwrite it
-document.styleSheets[0].insertRule('.resizer         { position: absolute; bottom: 0; opacity: 0.8; background-color: #F0F0F0; height: 5px; cursor: n-resize; z-index: 2; }', 0);
-document.styleSheets[0].insertRule('.resizer:hover   { background-color: #7D98B8; opacity: 0.3 }', 0);
-document.styleSheets[0].insertRule('.resizer .handle { background-color: white; border: 1px solid #5A5A5A; border-width: 1px 0; display: inline-block; height: 1px; margin: 1px 48%; width: 4%; }', 0);
+document.styleSheets[0].insertRule('body.dragging *                    { cursor: n-resize!important; }', 0);
+document.styleSheets[0].insertRule('.canvas_container .resizer         { position: absolute; bottom: 0; opacity: 0.8; background-color: #F0F0F0; height: 5px; cursor: n-resize; z-index: 2; }', 0);
+document.styleSheets[0].insertRule('.canvas_container .resizer.shadow  { box-shadow: 0 -3px 5px 0 #000000; }', 0);
+document.styleSheets[0].insertRule('.canvas_container .resizer .handle { background-color: white; border: 1px solid #5A5A5A; border-width: 1px 0; display: inline-block; height: 1px; margin: 1px 48%; width: 4%; }', 0);
+document.styleSheets[0].insertRule('.canvas_container .resizer:hover, .canvas_container .resizer.ui-draggable-dragging { box-shadow: 0 -5px 7px -3px #000000 inset!important; }', 0);
 
 CBrowse.Track.on('afterInit', function () {
   if (!this.resizable) {
@@ -13,12 +15,24 @@ CBrowse.Track.on('afterInit', function () {
   var track = this;
   
   this.resizer = (this.resizer || $('<div class="resizer"><div class="handle"></div></div>').appendTo(this.container).draggable({ 
-    axis : 'y', 
-    stop : function (e, ui) {
+    axis   : 'y',
+    start  : function () { $('body').addClass('dragging'); },
+    stop   : function (e, ui) {
+      $('body').removeClass('dragging');
       track.resize(track.height + ui.position.top - ui.originalPosition.top, true);
       $(this).css({ top: 'auto' });
     }
+  }).on('click', function () {
+    if (track.fullVizibleHeight) {
+      track.resize(track.fullVizibleHeight);
+    }
   })).css({ width: this.width, left: -this.cBrowse.left }).show();
+});
+
+CBrowse.Track.on('afterToggleExpander', function () {
+  if (this.resizer && this.expander) {
+    this.resizer[this.expander.filter(':visible').hide().length ? 'addClass' : 'removeClass']('shadow');
+  }
 });
 
 CBrowse.on('afterMove', function () {
