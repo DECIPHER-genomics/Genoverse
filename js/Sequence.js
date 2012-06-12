fb7d1df7b5a7720353841fea165509070eeea12e
Simon Brent <simon.brent@gmail.com>
2012-06-12 12:22:27 +0100

    Sequence track

---
+++ js/Sequence.js
@@ -0,0 +1,11 @@
+CBrowse.Track.Sequence = CBrowse.Track.extend({
+  config: {
+    labelOverlay   : true,
+    separateLabels : false,
+    fixedHeight    : false,
+    resizable      : false,
+    autoHeight     : true,
+    spacing        : 0,
+    inherit        : [ 'Stranded' ]
+  }
+});
