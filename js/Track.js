0bdf34849d7aa43cdf584647cc07b06ca6ff75e9
Simon Brent <simon.brent@gmail.com>
2012-06-13 17:07:13 +0100

    stupid boy.

--- js/Track.js
+++ js/Track.js
@@ -130,7 +130,9 @@ CBrowse.Track = Base.extend({
       var y       = e.pageY - $(e.target).offset().top;
       var feature = track[e.target.className === 'labels' ? 'labelPositions' : 'featurePositions'].search({ x: x, y: y, w: 1, h: 1 }).sort(function (a, b) { return a.sort - b.sort; })[0];
       
-      track.cBrowse.makeMenu(track, feature, { left: e.pageX, top: e.pageY });
+      if (feature) {
+        track.cBrowse.makeMenu(track, feature, { left: e.pageX, top: e.pageY });
+      }
     });
   },
   
