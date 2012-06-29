a83cc0b57e064618a28eb280cf0aba0d27a23b83
Eugene Bragin <eugene.bragin@gmail.com>
2012-06-29 13:23:11 +0100

    ErrorMessage

---
+++ js/Track/Error.js
@@ -0,0 +1,34 @@
+CBrowse.Track.Error = CBrowse.Track.extend({
+  config: {
+    color   : '#FF0000',
+    spacing : 0,
+    inherit : [ 'Static' ]
+  },
+  
+  constructor: function (config) {
+    this.base(config);
+    
+    this.container.hide();
+    this.label.hide();
+    
+    this.height = this.featuresHeight = this.fontHeight;
+  },
+  
+  resize: $.noop,
+  
+  positionFeatures: function () {
+    var width = this.context.measureText(this.message).width;
+    var fill  = {};
+    fill[this.color] = [[ 'fillText', [ this.message, (this.width - width) / 2, 0 ] ]];
+    
+    return { fill: fill };
+  },
+  
+  draw: function (trackImgContainer, message) {
+    this.message = message || 'Unknown error';
+
+    this.image.makeImage();
+    this.base(this.image);
+    this.image.container.children().addClass('static').appendTo(trackImgContainer).css({ marginTop: -this.height / 2, marginLeft: this.width - this.cBrowse.left });
+  }
+});
