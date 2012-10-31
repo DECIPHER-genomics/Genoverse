fe4b2ad0392f7218eeaedd33ed85d960996fcc05
Eugene Bragin <eugene.bragin@gmail.com>
2012-10-31 13:03:37 +0000

    Latest changes merged

--- js/beta.js
+++ js/beta.js
@@ -1338,8 +1338,7 @@ Genoverse.Track = Base.extend({
     for (var key in this) {
       if (typeof this[key] === 'object') deepCopy[key] = this[key];
     }
-    // Use jQuery.extend to deep-copy
-    $.extend(true, this, deepCopy);
+    this.extend($.extend(true, {}, deepCopy));
 
     // Use Base.extend to make any funciton in config have this.base
     this.extend(config);
@@ -2099,6 +2098,7 @@ Genoverse.Track = Base.extend({
     });
   }
 });
+
 
 
 
