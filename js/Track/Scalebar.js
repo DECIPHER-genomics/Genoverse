acdd3eeafaa583b072aa1bdb9637b6b099090127
Eugene Bragin <eugene.bragin@gmail.com>
2012-11-05 09:32:20 +0000

    * Scalebar revised for new structure

--- js/Track/Scalebar.js
+++ js/Track/Scalebar.js
@@ -1,7 +1,7 @@
 Genoverse.Track.Scalebar = Genoverse.Track.extend({
 
   height        : 20,
-  featureHeight : 3,
+  featureHeight : 6,
   spacing       : 0,
   color         : '#000000',
   autoHeight    : false,
@@ -22,59 +22,22 @@ Genoverse.Track.Scalebar = Genoverse.Track.extend({
   setScale: function () {
     this.base();
     
-    var length = this.browser.length;
-    var majorUnit, minorUnit, exponent, mantissa;
-    
-    if (length <= 51) {
-      majorUnit = 10;
-      minorUnit = 1;
-    } else {
-      exponent = Math.pow(10, Math.floor(Math.log(length) / Math.log(10)));
-      mantissa = length / exponent;
-      
-      if (mantissa < 1.2) {
-        majorUnit = exponent  / 10;
-        minorUnit = majorUnit / 5;
-      } else if (mantissa < 2.5) {
-        majorUnit = exponent  / 5;
-        minorUnit = majorUnit / 4;
-      } else if (mantissa < 5) {
-        majorUnit = exponent  / 2;
-        minorUnit = majorUnit / 5;
-      } else {
-        majorUnit = exponent;
-        minorUnit = majorUnit / 5;
-      }
-    }
-    
-    this.minorUnit  = this.browser.minorUnit = minorUnit;
-    this.majorUnit  = this.browser.majorUnit = majorUnit;
+    this.minorUnit  = this.browser.minorUnit;
+    this.majorUnit  = this.browser.majorUnit;
     this.seen       = {};
     this.features   = new RTree();
     this.featureIds = {};
-    
-    if (this.strand === 1) {
-      if (!this.browser.guideLinesByScale) {
-        this.browser.guideLinesByScale = {};
-      }
-      
-      if (!this.browser.guideLinesByScale[this.scale]) {
-        this.browser.guideLinesByScale[this.scale] = { major: {}, minor: {} };
-      }
-      
-      this.browser.guideLines = this.browser.guideLinesByScale[this.scale];
-    }
   },
   
   setFeatures: function (start, end) {
-    start = Math.max(start - (start % this.minorUnit) - this.majorUnit, 0);
+    var start = Math.max(start - (start % this.minorUnit) - this.majorUnit, 0);
     
     var flip     = (start / this.minorUnit) % 2 ? 1 : -1;
     var features = [];
     var feature, major, label;
     
     for (var x = start; x < end + this.minorUnit; x += this.minorUnit) {
-      flip *= -1;
+      //flip *= -1;
       
       if (this.seen[x]) {
         continue;
@@ -85,53 +48,26 @@ Genoverse.Track.Scalebar = Genoverse.Track.extend({
       feature = { id: x, strand: 1 };
       major   = x && !(x % this.majorUnit);
       
-      if (flip === 1) {
+      //if (flip === 1) {
         feature.start = x;
         feature.end   = x + this.minorUnit - 1;
         feature.color = this.color;
-      }
+      //}
       
       if (major) {
-        label = this.formatLabel(x);
-        
-        if (label !== this.lastLabel) {
-          feature.label      = label;
-          feature.labelColor = this.color;
-          
-          if (!feature.end) {
-            feature.start = x;
-            feature.end   = x;
-            feature.color = this.browser.colors.background;
-          }
-        }
-        
-        this.lastLabel = label;
+        feature.major = true;
+        feature.label = this.formatLabel(x);
       }
-      
-      if (feature.end) {
-        features.push(feature);
-      }
+
+      this.insertFeature(feature, feature.start, feature.end);
       
       if (this.strand === 1) {
         this.browser.guideLines[major ? 'major' : 'minor'][x] = Math.round(x * this.scale);
       }
     }
-    
-    return this.parseData({ features: features });
   },
-  
-  positionFeatures: function (features, startOffset, imageWidth) {
-    var features = this.base(features, startOffset, imageWidth);
-    
-    this.labels = $.grep(features.fill[this.color], function (f) { return f[0] === 'fillText'; });
-    
-    if (features.fill[this.browser.colors.background]) {
-      this.colorOrder = [ this.color, this.browser.colors.background ];
-    }
-    
-    return features;
-  },
-  
+
+
   makeReverseImage: function (start, end, width, moved, cls, img) {
     var div      = this.imgContainer.clone().width(width).addClass(cls).css('left', img.container.position().left).data('img', img);
     var deferred = $.Deferred();
@@ -149,28 +85,33 @@ Genoverse.Track.Scalebar = Genoverse.Track.extend({
     return deferred;
   },
   
-  getData: function (image, deferred) {
-    this.setFeatures(image.start, image.end);
-    this.base(image, deferred);
+
+  getData: function (start, end) {
+    this.setFeatures(start, end);
   },
-  
-  afterDraw: function (image) {
-    var i = this.labels.length;
-    
+
+
+  parseData: function () {},
+
+
+  draw: function (features, context) {
+    var i = features.length;
+
+    context.textBaseline = 'top';
+
     while (i--) {
-      this.context.fillRect(Math.round(this.labels[i][1][1]), this.featureHeight, 1, 3);
+      var feature = features[i];
+      if (feature.major) {
+        context.fillRect(Math.round(feature.scaledStart), 0, 1, this.featureHeight);
+        context.fillText(feature.label, feature.scaledStart, this.featureHeight);
+      } else {
+        context.fillRect(Math.round(feature.scaledStart), 0, 1, this.featureHeight/2);
+      }
     }
     
-    this.context.fillRect(0, 0,                  image.width, 1);
-    this.context.fillRect(0, this.featureHeight, image.width, 1);
+    context.fillRect(0, 0, context.canvas.width, 1);
+    //context.fillRect(0, this.featureHeight, context.canvas.width, 1);
   },
   
-  drawBackground: function () {
-    return this.strand === 1 ? this.base.apply(this, arguments) : false;
-  },
-  
-  drawBackgroundColor: function (image, height) {
-    this.context.fillStyle = this.browser.colors.background;
-    this.context.fillRect(0, 0, this.width, height);
-  }
+
 });
