16dc14d0404144e480999832e6ed325a0a492569
Eugene Bragin <eugene.bragin@gmail.com>
2013-04-16 17:15:35 +0100

    Playing with drag & drop

--- js/plugins/fileDrop.js
+++ js/plugins/fileDrop.js
@@ -2,12 +2,34 @@ Genoverse.on('afterInit', function() {
   var fileDropDiv = $('<div class="gv_file_drop" />').appendTo(this.wrapper);
   var container = this.container[0];
 
-  container.addEventListener("dragenter", function (e) { fileDropDiv.addClass('hover'); return false; }, true);
-  window.addEventListener("dragenter", function (e) { if (!$.contains(container, e.target)) fileDropDiv.removeClass('hover'); return false; }, false);
+  container.addEventListener("dragenter", function (e) { 
+    fileDropDiv.addClass('hover'); 
+    return false; 
+  }, true);
+
+  document.addEventListener("dragover", function( event ) {
+      // prevent default to allow drop
+      event.preventDefault();
+  }, false);
+
+  document.addEventListener("dragenter", function (e) { 
+    if (!$.contains(container, e.target)) 
+      fileDropDiv.removeClass('hover'); 
+    return false; 
+  }, false);
+
+  document.addEventListener("dragleave", function (e) {
+      // reset background of potential drop target when the draggable element leaves it
+      console.log(e);
+      //fileDropDiv.removeClass('hover'); 
+ 
+  }, false);
 
   container.ondrop      = function (e) {
     fileDropDiv.removeClass('hover');
     e.preventDefault();
+
+    console.log(e.dataTransfer.files);
 
     var file = e.dataTransfer.files[0], reader = new FileReader();
 
@@ -16,7 +38,6 @@ Genoverse.on('afterInit', function() {
       holder.style.background = 'url(' + event.target.result + ') no-repeat center';
     };
 
-    console.log(file);
     //reader.readAsDataURL(file);
 
     return false;
