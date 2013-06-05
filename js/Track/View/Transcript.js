cdfafbec93fc1799abc7ae6a81f5fa837586e214
EugeneBragin <eugene.bragin@gmail.com>
2013-06-06 00:26:24 +0100

    slight fixes for Transcript view

--- js/Track/View/Transcript.js
+++ js/Track/View/Transcript.js
@@ -8,37 +8,39 @@ Genoverse.Track.View.Transcript = Genoverse.Track.View.extend({
   lineWidth     : 0.5,
 
   drawFeature: function(transcript, featureContext, labelContext, scale) {
-    if (!transcript.exons || !transcript.exons.length) return;
+    if (transcript.exons && transcript.exons.length) {
+      var exons = transcript.exons.sort(function(a, b){ return a.start - b.start });
+      for (var i=0; i<exons.length; i++) {
+        var exon = exons[i];
+        featureContext.strokeStyle = exon.color || transcript.color || this.color;
+        featureContext.lineWidth   = 1;
+        featureContext.strokeRect(
+          transcript.x + (exon.start - transcript.start) * scale,
+          transcript.y + 1.5,
+          (exon.end - exon.start) * scale, 
+          transcript.height - 2
+        );
 
-    var exons = transcript.exons.sort(function(a, b){ return a.start - b.start });
-    for (var i=0; i<exons.length; i++) {
-      var exon = exons[i];
-      featureContext.strokeStyle = exon.color || transcript.color || this.color;
-      featureContext.lineWidth   = 1;
-      featureContext.strokeRect(
-        transcript.x + (exon.start - transcript.start) * scale,
-        transcript.y + 2.5,
-        (exon.end - exon.start) * scale, 
-        transcript.height - 4
-      );
-
-      if (i) this.drawIntron({
-        x: transcript.x + (exons[i-1].end - transcript.start) * scale,
-        y: transcript.y + transcript.height/2,
-        width: (exon.start - exons[i-1].end) * scale,
-        height: transcript.strand > 0 ? -transcript.height/2 : transcript.height/2,
-      }, featureContext);
+        if (i) this.drawIntron({
+          x: transcript.x + (exons[i-1].end - transcript.start) * scale,
+          y: transcript.y + transcript.height/2 + 0.5,
+          width: (exon.start - exons[i-1].end) * scale,
+          height: transcript.strand > 0 ? -transcript.height/2 : transcript.height/2,
+        }, featureContext);
+      }
     }
 
-    for (var i=0; i<transcript.cds.length; i++) {
-      var cds = transcript.cds[i];
-      featureContext.fillStyle = cds.color || transcript.color || this.color;
-      featureContext.fillRect(
-        transcript.x + (cds.start - transcript.start) * scale,
-        transcript.y, 
-        (cds.end - cds.start) * scale, 
-        transcript.height
-      );
+    if (transcript.cds && transcript.cds.length) {
+      for (var i=0; i<transcript.cds.length; i++) {
+        var cds = transcript.cds[i];
+        featureContext.fillStyle = cds.color || transcript.color || this.color;
+        featureContext.fillRect(
+          transcript.x + (cds.start - transcript.start) * scale,
+          transcript.y, 
+          (cds.end - cds.start) * scale, 
+          transcript.height
+        );
+      }
     }
 
     if (this.labels && transcript.label) {
