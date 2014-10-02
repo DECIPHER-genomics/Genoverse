d07a71a9a2030bff3c6957bb5d38aa96c4be1997
Simon Brent <sb23@sanger.ac.uk>
2014-10-02 11:49:48 +0100

    Added focusRegion plugin

---
+++ js/plugins/focusRegion.js
@@ -0,0 +1,14 @@
+Genoverse.Plugins.focusRegion = function () {
+  this.controls.push({
+    icon    : '&#8617;',
+    name    : 'Reset focus to ' + (this.focusRegion && this.focusRegion.name ? this.focusRegion.name : this.chr + ':' + this.start + '-' + this.end),
+    'class' : 'gv-button-large',
+    action  : function (browser) { browser.moveTo(browser.focusRegion.start, browser.focusRegion.end, true); }
+  });
+
+  this.on({
+    beforeInit: function () {
+      this.focusRegion = this.focusRegion || { start: this.start, end: this.end };
+    }
+  });
+};
