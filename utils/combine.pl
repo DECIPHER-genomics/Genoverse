5dd1da7282fb37b1ad25288ea2196d6f2dff690d
Simon Brent <sb23@sanger.ac.uk>
2015-03-30 14:21:46 +0100

    Updated to jQuery 2.1.3 and added genoverse.combined.nojquery.js file, if you already have jquery on your site

--- utils/combine.pl
+++ utils/combine.pl
@@ -6,39 +6,50 @@ use Data::Dumper;
 my @files;
 
 open INDEX, 'expanded.html' or die $!;
+
 while (<INDEX>) {
-  if ($_ =~ /<script[^>]+src="(.*)"/) {
-    push @files, $1;
-  }
+  push @files, $1 if /<script[^>]+src="(.*)"/;
 }
 
-open COMBINED, '>', 'js/genoverse.combined.js' or die $!;
+my $wrapper = q|(function () {
 
-print COMBINED "(function () {\n";
-for (@files) {
-  local $|=1;  
-  open my $FILE, '<', $_ or warn "Can't open '$_' for read: $!";
-  while (<$FILE>) {
-    print COMBINED;
-  }
-  close $FILE;
-  print COMBINED "\n\n\n\n";
+%s
+
+var config = $('script:last').text();
+
+if (config) {
+  try {
+    config = eval('('+ config +')');
+    $(document).ready(function(){
+      window.genoverse = new Genoverse(config);
+    });
+  } catch (e) {
+    throw('Configuration ERROR:' + e);
+  };
+}
+})();
+|;
+
+my ($jquery, $js);
+
+foreach (@files) {
+  my $file = $_ eq 'js/lib/jquery.js' ? \$jquery : \$js;
+
+  local $/ = undef;
+
+  open FH, '<', $_ or warn "Can't open '$_' for read: $!";
+
+  $$file .= <FH>;
+  $$file .= "\n\n";
+
+  close FH;
 }
 
-print COMBINED q|
-var thisScriptTag = $('script:last');          
-var config = thisScriptTag.text();             
-if (config) {                                  
-  try {                                        
-    config = eval('('+ config +')');           
-    $(document).ready(function(){              
-      window.genoverse = new Genoverse(config) 
-    });                                        
-  } catch (e) {                                
-    throw('Configuration ERROR:' + e);         
-  };                                           
-}                                              
-})();                                          
-$.noConflict(true);|;
+open COMBINED,           '>', 'js/genoverse.combined.js'          or die $!;
+open COMBINED_NO_JQUERY, '>', 'js/genoverse.combined.nojquery.js' or die $!;
+
+printf COMBINED           $wrapper, "$jquery$js";
+printf COMBINED_NO_JQUERY $wrapper, $js;
 
 close COMBINED;
+close COMBINED_NO_JQUERY;
