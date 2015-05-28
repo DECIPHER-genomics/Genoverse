3cabe29c26518d95c51ff059d8fb1d88459c1e1e
Simon Brent <sb23@sanger.ac.uk>
2015-05-28 16:44:06 +0100

    Made legends more easily extendable

--- js/genoverse.combined.js
+++ js/genoverse.combined.js
@@ -5346,27 +5346,102 @@ Genoverse.Track.HighlightRegion = Genoverse.Track.extend({
   })
 });
 
-Genoverse.Track.Legend = Genoverse.Track.Static.extend({
+Genoverse.Track.Controller.Legend = Genoverse.Track.Controller.Static.extend({
+  init: function () {
+    this.base();
+
+    this.container.addClass('gv-track-container-legend');
+
+    if (!this.browser.legends) {
+      this.browser.legends = {};
+    }
+
+    this.browser.legends[this.track.id] = this;
+    this.track.setTracks();
+  },
+
+  destroy: function () {
+    delete this.browser.legends[this.id];
+    this.base();
+  }
+});
+
+Genoverse.Track.Model.Legend = Genoverse.Track.Model.Static.extend({
+  findFeatures: function () {
+    var bounds   = { x: this.browser.scaledStart, y: 0, w: this.width };
+    var features = {};
+
+    $.each($.map(this.track.tracks, function (track) {
+      var featurePositions = track.prop('featurePositions');
+      bounds.h = track.prop('height');
+      return featurePositions ? featurePositions.search(bounds).concat(track.prop('labelPositions').search(bounds)) : [];
+    }), function () {
+      if (this.legend) {
+        features[this.legend] = this.color;
+      }
+    });
+
+    // sort legend alphabetically
+    return $.map(features, function (color, text) { return [[ text, color ]]; }).sort(function (a, b) {
+      var x = a[0].toLowerCase();
+      var y = b[0].toLowerCase();
+      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+  }
+});
+
+Genoverse.Track.View.Legend = Genoverse.Track.View.Static.extend({
   textColor     : '#000000',
   labels        : 'overlay',
-  unsortable    : true,
-  lockToTrack   : true,  // Always put the legend just below the last track that the legend is for
   featureHeight : 12,
 
-  controller: Genoverse.Track.Controller.Static.extend({
-    init: function () {
-      this.base();
-
-      this.container.addClass('gv-track-container-legend');
-
-      if (!this.browser.legends) {
-        this.browser.legends = {};
-      }
-
-      this.browser.legends[this.track.id] = this;
-      this.track.setTracks();
+  positionFeatures: function (f, params) {
+    if (params.positioned) {
+      return f;
     }
-  }),
+
+    var cols     = 2;
+    var pad      = 5;
+    var w        = 20;
+    var x        = 0;
+    var y        = 0;
+    var xScale   = this.width / cols;
+    var yScale   = this.fontHeight + pad;
+    var features = [];
+    var xPos, yPos, labelWidth;
+
+    for (var i = 0; i < f.length; i++) {
+      xPos       = (x * xScale) + pad;
+      yPos       = (y * yScale) + pad;
+      labelWidth = this.context.measureText(f[i][0]).width;
+
+      features.push(
+        { x: xPos,           y: yPos, width: w,              height: this.featureHeight, color: f[i][1] },
+        { x: xPos + pad + w, y: yPos, width: labelWidth + 1, height: this.featureHeight, color: false, labelColor: this.textColor, labelWidth: labelWidth, label: f[i][0] }
+      );
+
+      if (++x === cols) {
+        x = 0;
+        y++;
+      }
+    }
+
+    params.height     = this.prop('height', f.length ? ((y + (x ? 1 : 0)) * yScale) + pad : 0);
+    params.width      = this.width;
+    params.positioned = true;
+
+    return this.base(features, params);
+  }
+});
+
+Genoverse.Track.Legend = Genoverse.Track.Static.extend({
+  unsortable  : true,
+  lockToTrack : true, // Always put the legend just below the last track that the legend is for
+  removable   : false,
+
+  controller : Genoverse.Track.Controller.Legend,
+  model      : Genoverse.Track.Model.Legend,
+  view       : Genoverse.Track.View.Legend,
 
   setEvents: function () {
     this.browser.on({
@@ -5443,66 +5518,6 @@ Genoverse.Track.Legend = Genoverse.Track.Static.extend({
     this.browser.sortTracks();
   },
 
-  findFeatures: function () {
-    var bounds   = { x: this.browser.scaledStart, y: 0, w: this.width };
-    var features = {};
-
-    $.each($.map(this.track.tracks, function (track) {
-      var featurePositions = track.prop('featurePositions');
-      bounds.h = track.prop('height');
-      return featurePositions ? featurePositions.search(bounds).concat(track.prop('labelPositions').search(bounds)) : [];
-    }), function () {
-      if (this.legend) {
-        features[this.legend] = this.color;
-      }
-    });
-
-    // sort legend alphabetically
-    return $.map(features, function (color, text) { return [[ text, color ]]; }).sort(function (a, b) {
-      var x = a[0].toLowerCase();
-      var y = b[0].toLowerCase();
-      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    });
-  },
-
-  positionFeatures: function (f, params) {
-    if (params.positioned) {
-      return f;
-    }
-
-    var cols     = 2;
-    var pad      = 5;
-    var w        = 20;
-    var x        = 0;
-    var y        = 0;
-    var xScale   = this.width / cols;
-    var yScale   = this.fontHeight + pad;
-    var features = [];
-    var xPos, yPos, labelWidth;
-
-    for (var i = 0; i < f.length; i++) {
-      xPos       = (x * xScale) + pad;
-      yPos       = (y * yScale) + pad;
-      labelWidth = this.context.measureText(f[i][0]).width;
-
-      features.push(
-        { x: xPos,           y: yPos, width: w,              height: this.featureHeight, color: f[i][1] },
-        { x: xPos + pad + w, y: yPos, width: labelWidth + 1, height: this.featureHeight, color: false, labelColor: this.textColor, labelWidth: labelWidth, label: f[i][0] }
-      );
-
-      if (++x === cols) {
-        x = 0;
-        y++;
-      }
-    }
-
-    params.height     = this.prop('height', f.length ? ((y + (x ? 1 : 0)) * yScale) + pad : 0);
-    params.width      = this.width;
-    params.positioned = true;
-
-    return this.base(features, params);
-  },
-
   enable: function () {
     this.base();
     this.controller.makeImage({});
@@ -5510,11 +5525,6 @@ Genoverse.Track.Legend = Genoverse.Track.Static.extend({
 
   disable: function () {
     delete this.controller.stringified;
-    this.base();
-  },
-
-  destroy: function () {
-    delete this.browser.legends[this.id];
     this.base();
   }
 });
