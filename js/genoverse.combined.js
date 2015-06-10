8db85f9b841ed19762e3464b3f784d4eaae436b4
Simon Brent <sb23@sanger.ac.uk>
2015-06-10 09:26:06 +0100

    Fixed off-by-one error in transcript track

--- js/genoverse.combined.js
+++ js/genoverse.combined.js
@@ -4742,40 +4742,40 @@ Genoverse.Track.View.Transcript = Genoverse.Track.View.extend({
   bump          : true,
   intronStyle   : 'bezierCurve',
   lineWidth     : 0.5,
-  
-  drawFeature: function(transcript, featureContext, labelContext, scale) {
+
+  drawFeature: function (transcript, featureContext, labelContext, scale) {
     this.setFeatureColor(transcript);
-    
+
     var exons = (transcript.exons || []).sort(function (a, b) { return a.start - b.start; });
     var exon, cds, i;
-    
+
     if (!exons.length || exons[0].start > transcript.start) {
       exons.unshift({ start: transcript.start, end: transcript.start });
     }
-    
+
     if (!exons.length || exons[exons.length - 1].end < transcript.end) {
       exons.push({ start: transcript.end, end: transcript.end  });
     }
-    
+
     for (i = 0; i < exons.length; i++) {
       exon = exons[i];
-      
+
       featureContext.strokeStyle = exon.color || transcript.color || this.color;
       featureContext.lineWidth   = 1;
-      
+
       featureContext.strokeRect(
         transcript.x + (exon.start - transcript.start) * scale,
         transcript.y + 1.5,
-        Math.max(1, (exon.end - exon.start) * scale), 
+        Math.max(1, (exon.end - exon.start) * scale),
         transcript.height - 3
       );
-      
+
       if (i) {
         this.drawIntron({
-          x: transcript.x + (exons[i - 1].end - transcript.start) * scale,
-          y: transcript.y + transcript.height / 2 + 0.5,
-          width: (exon.start - exons[i - 1].end) * scale,
-          height: transcript.strand > 0 ? -transcript.height / 2 : transcript.height / 2
+          x      : transcript.x + (exons[i - 1].end - transcript.start + 1) * scale,
+          y      : transcript.y + transcript.height / 2 + 0.5,
+          width  : (exon.start - exons[i - 1].end + 1) * scale,
+          height : transcript.strand > 0 ? -transcript.height / 2 : transcript.height / 2
         }, featureContext);
       }
     }
@@ -4783,27 +4783,27 @@ Genoverse.Track.View.Transcript = Genoverse.Track.View.extend({
     if (transcript.cds && transcript.cds.length) {
       for (i = 0; i < transcript.cds.length; i++) {
         cds = transcript.cds[i];
-        
+
         featureContext.fillStyle = cds.color || transcript.color || this.color;
-        
+
         featureContext.fillRect(
           transcript.x + (cds.start - transcript.start) * scale,
-          transcript.y, 
-          Math.max(1, (cds.end - cds.start) * scale),
+          transcript.y,
+          Math.max(1, (cds.end - cds.start + 1) * scale),
           transcript.height
         );
       }
     }
-    
+
     if (this.labels && transcript.label) {
       this.drawLabel(transcript, labelContext, scale);
     }
-  },  
-  
+  },
+
   drawIntron: function (intron, context) {
     context.beginPath();
     context.lineWidth = this.lineWidth;
-    
+
     switch (this.intronStyle) {
       case 'line':
         context.moveTo(intron.x, intron.y);
@@ -4820,7 +4820,7 @@ Genoverse.Track.View.Transcript = Genoverse.Track.View.extend({
         break;
       default: break;
     }
-    
+
     context.stroke();
     context.closePath();
   }
