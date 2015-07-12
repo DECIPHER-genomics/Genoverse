6dd58ad4e2fc49781b212ee90ab5c4db4a2f380b
Simon Brent <sb23@sanger.ac.uk>
2015-07-12 13:33:24 +0100

    Added strand indicator to gene labels

--- js/Track/Model/Gene/Ensembl.js
+++ js/Track/Model/Gene/Ensembl.js
@@ -2,18 +2,18 @@
 Genoverse.Track.Model.Gene.Ensembl = Genoverse.Track.Model.Gene.extend({
   url              : '//rest.ensembl.org/overlap/region/human/__CHR__:__START__-__END__?feature=gene;content-type=application/json',
   dataRequestLimit : 5000000, // As per e! REST API restrictions
-  
+
   // The url above responds in json format, data is an array
   // We assume that parents always preceed children in data array, gene -> transcript -> exon
   // See rest.ensembl.org/documentation/info/feature_region for more details
   parseData: function (data) {
     for (var i = 0; i < data.length; i++) {
       var feature = data[i];
-      
+
       if (feature.feature_type === 'gene' && !this.featuresById[feature.id]) {
-        feature.label       = feature.external_name || feature.id;
+        feature.label       = parseInt(feature.strand, 10) === 1 ? (feature.external_name || feature.id) + ' >' : '< ' + (feature.external_name || feature.id);
         feature.transcripts = [];
-        
+
         this.insertFeature(feature);
       }
     }
