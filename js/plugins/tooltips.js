e907f78ed58fadfb2263243b085fa453fcba6350
Simon Brent <sb23@sanger.ac.uk>
2015-07-17 15:27:05 +0100

    Fixes for tooltips and fullscreen

--- js/plugins/tooltips.js
+++ js/plugins/tooltips.js
@@ -4,25 +4,75 @@
 // released under the MIT license
 
 Genoverse.Plugins.tooltips = function () {
+  var browser = this;
+
+  function toggleTooltips(browser, tooltips, action) {
+    var offset = browser.superContainer.offset();
+
+    tooltips = tooltips || browser.superContainer.find('.gv-tooltip');
+    action   = action   || $(this).toggleClass('gv-active').hasClass('gv-active') ? 'show' : 'hide';
+
+    tooltips.each(function () {
+      $(this).tipsy(action).data('tipsy').$tip.appendTo(browser.superContainer).css({ marginTop: -offset.top, marginLeft: -offset.left });
+    });
+  }
+
+  function updateTooltips() {
+    var tooltips = $();
+
+    $.each([
+      [ browser.labelContainer.find('.gv-handle'), { gravity: 'w', fade: true, trigger: 'manual', fallback: 'Reorder tracks by dragging this handle' }],
+      [ browser.container.find('.gv-resizer'),     { gravity: 'n', fade: true, trigger: 'manual', fallback: 'Resize track by dragging this handle'   }],
+    ], function () {
+      var el = this[0].first();
+
+      if (!el.hasClass('gv-tooltip')) {
+        this[0].filter('.gv-tooltip').removeClass('gv-tooltip').tipsy('hide').removeData('tipsy');
+        el.tipsy(this[1]).addClass('gv-tooltip');
+      }
+
+      tooltips = tooltips.add(el);
+    });
+
+    if (browser.controlPanel.find('.gv-tooltips').hasClass('gv-active')) {
+      toggleTooltips(browser, tooltips, 'show');
+    }
+
+    return tooltips;
+  }
+
   this.controls.push({
     icon    : '<i class="fa fa-info-circle"></i>',
-    'class' : 'gv-tooltip',
+    'class' : 'gv-tooltips',
     name    : 'Tooltips',
-    action  : function (browser) {
-      var func = $(this).toggleClass('gv-active').hasClass('gv-active') ? 'show' : 'hide';
-      browser.superContainer.find('.gv-tooltip').not(this).each(function () { $(this).tipsy(func); });
-    }
+    action  : toggleTooltips
   });
 
   this.on('afterInit', function () {
-    this.superContainer.find('.gv-panel .gv-button-set[title]').tipsy({ gravity: 'w', fade: true, trigger: 'manual' }).addClass('gv-tooltip');
-    this.container.find('.resizer:last').tipsy({ gravity: 'n', fade: true, trigger: 'manual', fallback: 'Resize track by dragging this handle' }).addClass('gv-tooltip');
-    this.wrapper.tipsy({ gravity: 's', fade: true, trigger: 'manual', fallback: 'Scroll left and right by dragging with your mouse, click on any feature in any track for more info' }).addClass('genoverse-tooltip');
-    this.labelContainer.tipsy({ gravity: 'e', fade: true, trigger: 'manual', fallback: 'Reorder tracks by dragging this handle' }).addClass('gv-tooltip');
+    this.superContainer.find('.gv-panel-left  .gv-button-set[title]').tipsy({ gravity: 'w', fade: true, trigger: 'manual' }).addClass('gv-tooltip');
+    this.superContainer.find('.gv-panel-right .gv-button-set[title]').tipsy({ gravity: 'e', fade: true, trigger: 'manual' }).addClass('gv-tooltip');
+
+    // In order to force placement of this tooltip to be inside the superContainer boundaries, and just below the karyotype, create a hidden element, positioned where we want the tooltip to appear
+    $('<i class="gv-wrapper-tooltip">').prependTo(this.wrapper).tipsy({
+      gravity  : 's',
+      fade     : true,
+      trigger  : 'manual',
+      fallback : 'Scroll left and right by dragging with your mouse, click on any feature in any track for more info'
+    }).addClass('gv-tooltip');
+
+    updateTooltips();
+  });
+
+  this.on('beforeSetWidth', function () {
+    this.controlPanel.find('.gv-tooltips.gv-active').trigger('click');
+  });
+
+  this.on('afterSortTracks', function () {
+    updateTooltips();
   });
 
   this.on('afterResize', 'tracks', function () {
-    this.container.find('.gv-resizer:last').tipsy('hide');
+    updateTooltips();
   });
 };
 
