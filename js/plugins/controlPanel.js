7c26b88df0b4da85563950ff66242b2fcf2cc12a
Simon Brent <sb23@sanger.ac.uk>
2015-11-30 16:46:11 +0000

    Fixed track ordering in control panel

--- js/plugins/controlPanel.js
+++ js/plugins/controlPanel.js
@@ -256,7 +256,7 @@ Genoverse.Plugins.controlPanel = function () {
             currentTracks.data('listTracks')();
 
             if (browser.tracksLibrary && browser.tracksLibrary.length) {
-              var tracksLibrary = $.map(browser.tracksLibrary, function (track) { return track.prototype.name ? [[ track.prototype.name.toLowerCase(), track ]] : undefined }).sort(function (a, b) { return b < a });
+              var tracksLibrary = $.map(browser.tracksLibrary, function (track) { return track.prototype.name ? [[ track.prototype.name.toLowerCase(), track ]] : undefined }).sort(function (a, b) { return a[0] > b[0] ? 1 : -1 });
 
               for (var i = 0; i < tracksLibrary.length; i++) {
                 (function (track) {
