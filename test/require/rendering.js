217cddd9a429808a3ff84bda30bb07d00fdb3d7c
Simon Brent <sb23@sanger.ac.uk>
2016-06-30 16:03:35 +0100

    Tests for transcripts

--- test/require/rendering.js
+++ test/require/rendering.js
@@ -28,75 +28,81 @@ function getDrawingInstructions(draw, region, type) {
   return instructions;
 }
 
-global.testTrackRender = function (features, track, draw, props) {
-  var deferreds = [];
+function getTrackConfig(features, draw) {
+  return {
+    data: features,
 
-  new Genoverse($.extend({
+    beforeMakeImage: function (params) {
+      params._testDeferred = Array(this.prop('labels') === 'separate' ? 2 : 1).fill($.Deferred());
+      this.browser._testDeferreds = this.browser._testDeferreds.concat(params._testDeferred);
+    },
+
+    afterRender: function (f, image) {
+      var track    = this;
+      var separate = this.prop('labels') === 'separate' && image.next().length;
+
+      (separate ? [ image, image.next() ] : [ image ]).forEach(function (img, i) {
+        var canvas  = document.createElement('canvas');
+        var context = canvas.getContext('2d');
+        var region  = img.data('start') + '-' + img.data('end');
+
+        canvas.width  = img.data('width');
+        canvas.height = img.data(separate && i === 1 ? 'labelHeight' : 'featureHeight');
+
+        if (track.prop('labels') === 'overlay') {
+          context.textAlign    = 'center';
+          context.textBaseline = 'middle';
+        } else {
+          context.textAlign    = 'left';
+          context.textBaseline = 'top';
+        }
+
+        context.font      = track.prop('font');
+        context.lineWidth = 1;
+
+        getDrawingInstructions(draw, region, separate ? i ? 'labels' : 'features' : '').forEach(function (instruction) {
+          if (typeof instruction === 'function') {
+            return instruction(context);
+          }
+
+          var func   = typeof instruction[0] === 'string' ? instruction.shift() : 'fillRect';
+          var fill   = /fill/.test(func);
+          var style  = fill ? 'fillStyle' : 'strokeStyle';
+          var color;
+
+          if (typeof instruction[instruction.length - 1] === 'string') {
+            color = instruction.pop();
+          } else {
+            color = (fill ? '' : track.prop('borderColor')) || track.prop('color') || 'black';
+          }
+
+          context[style] = color;
+          context[func].apply(context, instruction);
+        });
+
+        expect(canvas.toDataURL()).toEqual(img[0].src, 'Drawing is incorrect for ' + region + (separate ? i === 1 ? ' labels' : ' features' : ''));
+
+        img.data('_testDeferred').shift().resolve();
+      });
+    }
+  };
+};
+
+global.testTrackRender = function (features, track, draw, genoverseConfig) {
+  var trackConfig = $.extend(track || {}, getTrackConfig(features, draw));
+  var genoverse   = new Genoverse($.extend({
     chr            : 1,
     start          : 1,
     end            : 20,
     width          : 100,
     chromosomeSize : 1000,
-    tracks         : [
-      Genoverse.Track.extend($.extend(track || {}, {
-        data: features,
+    _testDeferreds : [],
+    tracks         : [ (trackConfig._testClass || Genoverse.Track).extend(trackConfig) ]
+  }, genoverseConfig || {}));
 
-        beforeMakeImage: function (params) {
-          params._testDeferred = Array(this.prop('labels') === 'separate' ? 2 : 1).fill($.Deferred());
-          deferreds = deferreds.concat(params._testDeferred);
-        },
-
-        afterRender: function (f, image) {
-          var track    = this;
-          var separate = this.prop('labels') === 'separate' && image.next().length;
-
-          (separate ? [ image, image.next() ] : [ image ]).forEach(function (img, i) {
-            var canvas  = document.createElement('canvas');
-            var context = canvas.getContext('2d');
-            var region  = img.data('start') + '-' + img.data('end');
-
-            canvas.width  = img.data('width');
-            canvas.height = img.data(separate && i === 1 ? 'labelHeight' : 'featureHeight');
-
-            if (track.prop('labels') === 'overlay') {
-              context.textAlign    = 'center';
-              context.textBaseline = 'middle';
-            } else {
-              context.textAlign    = 'left';
-              context.textBaseline = 'top';
-            }
-
-            context.font      = track.prop('font');
-            context.lineWidth = 1;
-
-            getDrawingInstructions(draw, region, separate ? i ? 'labels' : 'features' : '').forEach(function (instruction) {
-              var func   = typeof instruction[0] === 'string' ? instruction.shift() : 'fillRect';
-              var stroke = /stroke/.test(func);
-              var style  = stroke ? 'strokeStyle' : 'fillStyle';
-              var color;
-
-              if (typeof instruction[instruction.length - 1] === 'string') {
-                color = instruction.pop();
-              } else {
-                color = (stroke ? track.prop('borderColor') : '') || track.prop('color') || 'black';
-              }
-
-              context[style] = color;
-              context[func].apply(context, instruction);
-            });
-
-            expect(canvas.toDataURL()).toEqual(img[0].src, 'Drawing is incorrect for ' + region + (separate ? i === 1 ? ' labels' : ' features' : ''));
-
-            img.data('_testDeferred').shift().resolve();
-          });
-        }
-      }))
-    ]
-  }, props || {}));
-
-  return $.when.apply($, deferreds);
+  return $.when.apply($, genoverse._testDeferreds);
 };
 
-global.testTrackRenderStatic = function (features, track, draw, props) {
-  return testTrackRender(features, track, draw, $.extend(props || {}, { isStatic: true }));
+global.testTrackRenderStatic = function (features, track, draw, genoverseConfig) {
+  return testTrackRender(features, track, draw, $.extend(genoverseConfig || {}, { isStatic: true }));
 };
