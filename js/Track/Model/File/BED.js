0b0b344f07a876617a527f78f0eb435f70d6dd59
sourabh2k15 <sourab.reddy2k14@gmail.com>
2017-07-28 07:50:52 +0530

    BED parsing and rendering

--- js/Track/Model/File/BED.js
+++ js/Track/Model/File/BED.js
@@ -1,16 +1,29 @@
 Genoverse.Track.Model.File.BED = Genoverse.Track.Model.File.extend({
   parseData: function (text, chr) {
     var lines = text.split('\n');
-
     for (var i = 0; i < lines.length; i++) {
       var fields = lines[i].split('\t');
 
-      if (fields.length < 3) {
+      if (fields.length < 3 || fields[0] == 'track' || fields[0] == 'browser') {
         continue;
       }
+      var len = fields.length;
 
       if (fields[0] == chr || fields[0].toLowerCase() == 'chr' + chr || fields[0].match('[^1-9]' + chr + '$')) {
-        var score = parseFloat(fields[4], 10);
+        var feature = {};
+        feature.chr = chr;
+        feature.start = parseInt(fields[1], 10);
+        feature.end   = parseInt(fields[2], 10);
+        feature.name  = fields[3];
+
+        if (len > 3) feature.score = parseFloat(fields[4], 10);
+        if (len > 5) feature.strand = fields[5];
+
+        if (len > 7) {
+          feature.thickStart = fields[6];
+          feature.thickEnd   = fields[7];
+          if(feature.thickEnd == feature.thickStart == feature.chromStart) feature.drawThickBlock = false;
+        }
         var color = '#000000';
 
         if (fields[8]) {
@@ -19,15 +32,27 @@ Genoverse.Track.Model.File.BED = Genoverse.Track.Model.File.extend({
           color = this.scoreColor(isNaN(score) ? 1000 : score);
         }
 
-        this.insertFeature({
-          chr             : chr,
-          start           : parseInt(fields[1], 10),
-          end             : parseInt(fields[2], 10),
-          id              : chr + ':' + fields[1] + '-' + fields[3],
-          label           : fields[3],
-          color           : color,
-          originalFeature : fields
-        });
+        if(len == 12){ //subfeatures present
+          feature.blockCount = parseInt(fields[9],10);
+          var blockSizes = fields[10].split(",");
+          var blockStarts = fields[11].split(",");
+          blockSizes.pop();
+          blockStarts.pop();
+          var subfeatures = [];
+
+          for(var i = 0; i < blockSizes.length; i++){
+            var subfeature = {};
+            subfeature.start = feature.start + parseInt(blockStarts[i]);
+            subfeature.end = subfeature.start + parseInt(blockSizes[i]);
+            subfeature.height = 7;
+            subfeature.color = 'black';
+            subfeatures.push(subfeature);
+          }
+
+          feature.subFeatures = subfeatures;
+        }
+        console.log(feature);
+        this.insertFeature(feature);
       }
     }
   },
