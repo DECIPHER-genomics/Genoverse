6e3089fe4161dd7deb09d999fa376200001bd04f
Simon Brent <sb23@sanger.ac.uk>
2017-08-04 14:08:24 +0100

    Get webpack to create a minified file that works

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
-var webpack = require("webpack");
+var webpack = require('webpack');
 
 module.exports = {
-  entry   : __dirname + "/index.js",
-  output  : { filename: "js/genoverse.min.js" },
+  entry   : __dirname + '/index.js',
+  output  : { filename: 'js/genoverse.min.js' },
   plugins : [
     new webpack.ProvidePlugin({
-      $      : "jquery",
-      jQuery : "jquery"
+      $      : __dirname + '/js/lib/jquery.js',
+      jQuery : __dirname + '/js/lib/jquery.js'
+    }),
+    new webpack.DefinePlugin({
+      define: undefined // Stop jquery-ui.js trying to do define(["jquery"]), which doesn't work if jquery isn't in node_modules
     }),
     new webpack.optimize.UglifyJsPlugin({
       comments: false
