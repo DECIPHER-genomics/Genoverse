77936dc2daacb78cbc6d0a7b49524437715571f0
sourabh2k15 <sourab.reddy2k14@gmail.com>
2017-08-07 14:38:51 +0530

    implemented code changes as per review #2

--- js/Track/Model/File/WIG.js
+++ js/Track/Model/File/WIG.js
@@ -10,7 +10,7 @@ Genoverse.Track.Model.File.WIG = Genoverse.Track.Model.Graph.Bar.extend({
   },
   parseData : function(text, chr, s, e){
     var lines = text.split('\n');
-    var chrom, start, step, span, line, type;
+    var chrom, start, step, span, line;
     var features = [];
 
     while(lines.length && (line = lines.shift()) ){
@@ -19,46 +19,40 @@ Genoverse.Track.Model.File.WIG = Genoverse.Track.Model.Graph.Bar.extend({
     }
 
     if(line){
-      var fields = line.split(' ');
+      var fields = line.split(/\s+/);
       chrom = parseInt(fields[1].split("=")[1].replace("chr",""));
 
       if(fields[0] == 'fixedStep'){
-        type = 3;
         start = parseInt(fields[2].split("=")[1]);
         step  = parseInt(fields[3].split("=")[1]);
         span  = fields[4] ? parseInt(fields[4].split("=")[1]) : 1;
+
+        for(var i = 0; i < lines.length; i++){
+          var feature    = {};
+          feature.chr    = chrom;
+          feature.start  = start;
+          feature.end    = start + span;
+          feature.height = parseFloat(lines[i]);
+          features.push(feature);
+          start += step;
+        }
       }
       else if(fields[0] == 'variableStep'){
-        type = 2;
         span  = fields[2] ? parseInt(fields[2].split("=")[1]) : 1;
-      }
 
-    }
+        for(var i = 0; i < lines.length; i++){
+          var fields = lines[i].split(/\s+/);
 
-    if(type == 3){ //fixedStep parsing
-      for(var i = 0; i < lines.length; i++){
-        var feature    = {};
-        feature.chr    = chrom;
-        feature.start  = start;
-        feature.end    = start + span;
-        feature.height = parseFloat(lines[i]);
-        features.push(feature);
-        start += step;
+          var feature    = {};
+          feature.chr    = chrom;
+          feature.start  = parseInt(fields[0], 10);
+          feature.end    = feature.start + span;
+          feature.height = parseFloat(fields[1]);
+          features.push(feature);
+        }
       }
     }
-    else if(type == 2){ //variableStep parsing
-      for(var i = 0; i < lines.length; i++){
-        var fields = lines[i].split('\t');
 
-        var feature    = {};
-        feature.chr    = chrom;
-        feature.start  = parseInt(fields[0], 10);
-        feature.end    = feature.start + span;
-        feature.height = parseFloat(fields[1]);
-        features.push(feature);
-      }
-    }
-    console.log(features);
     return this.base.call(this, features, chr, s, e);
   }
 });
