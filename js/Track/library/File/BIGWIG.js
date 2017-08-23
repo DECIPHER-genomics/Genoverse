0c0c125c595d188d8a71423680277573a299179b
Simon Brent <sb23@sanger.ac.uk>
2017-08-23 08:12:24 +0100

    Allow bigwig tracks to switch between bar and line graph rendering

--- js/Track/library/File/BIGWIG.js
+++ js/Track/library/File/BIGWIG.js
@@ -1,7 +1,46 @@
 Genoverse.Track.File.BIGWIG = Genoverse.Track.Graph.Bar.extend({
-  model  : Genoverse.Track.Model.File.BIGWIG,
   name   : 'bigwig',
-  height : 100
+  height : 100,
+
+  setDefaults: function () {
+    this.bwReader = null; // Not part of model since it needs to be shared between bar and line graphs
+    this.base.apply(this, arguments);
+  },
+
+  getData: function (chr, start, end) {
+    var model    = this;
+    var deferred = $.Deferred();
+
+    if (!this.bigwigFile) {
+      this.bigwigFile = this.bigwigFile || (this.url ? new dallianceLib.URLFetchable(this.url) : new dallianceLib.BlobFetchable(this.track.dataFile));
+    }
+
+    var d = $.Deferred().done(function () {
+      model.prop('bwReader').getValues(chr, start, end, function (features, error) {
+        if (!error) {
+          features.sort(function (a, b) { return a.start - b.start; });
+          model.receiveData(features, chr, features[0].start, features[features.length - 1].end);
+          deferred.resolveWith(model);
+        }
+      });
+    });
+
+    if (this.prop('bwReader')) {
+      d.resolve();
+    } else {
+      new BWReader(this.bigwigFile, function (bwReader) {
+        if (bwReader) {
+          model.prop('bwReader', bwReader);
+          d.resolve();
+        } else {
+          model.receiveData([], chr, start, end);
+          return deferred.resolveWith(model);
+        }
+      });
+    }
+
+    return deferred;
+  }
 });
 
 Genoverse.Track.File.BW = Genoverse.Track.File.BIGWIG;
