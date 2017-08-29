8bbf8eaaa9fa2bb85edce2284d8959e69ea4c4da
Simon Brent <sb23@sanger.ac.uk>
2017-08-29 10:42:20 +0100

    Tidying

--- js/Track/Model/File/WIG.js
+++ js/Track/Model/File/WIG.js
@@ -1,53 +1,62 @@
 Genoverse.Track.Model.File.WIG = Genoverse.Track.Model.Graph.Bar.extend({
-  dataType : 'text',
-  getData : function(chr, start, end){
-    if(!this.url){
-      this.isLocal = true;
+  dataType: 'text',
+
+  getData: function () {
+    if (!this.url) {
+      this.isLocal  = true;
       this.dataFile = this.track.dataFile;
+
       return Genoverse.Track.Model.File.prototype.getData.apply(this, arguments);
     }
+
     return this.base.apply(this, arguments);
   },
-  parseData : function(text, chr, s, e){
-    var lines = text.split('\n');
-    var chrom, start, step, span, line;
-    var features = [];
 
-    while(lines.length && (line = lines.shift()) ){
-      if(line.indexOf('#') != -1 || line.indexOf('browser') != -1 || line.indexOf('track') != -1) continue;
-      else break;
+  parseData: function (text, chr, s, e) {
+    var lines    = text.split('\n');
+    var features = [];
+    var fields, chrom, start, step, span, line, feature, i;
+
+    while (lines.length && (line = lines.shift())) {
+      if (line.indexOf('#') != -1 || line.indexOf('browser') != -1 || line.indexOf('track') != -1) {
+        continue;
+      } else {
+        break;
+      }
     }
 
-    if(line){
-      var fields = line.split(/\s+/);
-      chrom = parseInt(fields[1].split("=")[1].replace("chr",""));
+    if (line) {
+      fields = line.split(/\s+/);
+      chrom  = parseInt(fields[1].split('=')[1].replace('chr',''));
 
-      if(fields[0] == 'fixedStep'){
-        start = parseInt(fields[2].split("=")[1]);
-        step  = parseInt(fields[3].split("=")[1]);
-        span  = fields[4] ? parseInt(fields[4].split("=")[1]) : 1;
+      if (fields[0] == 'fixedStep') {
+        start = parseInt(fields[2].split('=')[1]);
+        step  = parseInt(fields[3].split('=')[1]);
+        span  = fields[4] ? parseInt(fields[4].split('=')[1]) : 1;
 
-        for(var i = 0; i < lines.length; i++){
-          var feature    = {};
-          feature.chr    = chrom;
-          feature.start  = start;
-          feature.end    = start + span;
-          feature.height = parseFloat(lines[i]);
-          features.push(feature);
+        for (i = 0; i < lines.length; i++){
+          features.push({
+            chr    : chrom,
+            start  : start,
+            end    : start + span,
+            height : parseFloat(lines[i])
+          });
+
           start += step;
         }
-      }
-      else if(fields[0] == 'variableStep'){
-        span  = fields[2] ? parseInt(fields[2].split("=")[1]) : 1;
+      } else if (fields[0] == 'variableStep') {
+        span = fields[2] ? parseInt(fields[2].split('=')[1]) : 1;
 
-        for(var i = 0; i < lines.length; i++){
-          var fields = lines[i].split(/\s+/);
+        for (i = 0; i < lines.length; i++){
+          fields  = lines[i].split(/\s+/);
+          feature = {
+            chr    : chrom,
+            start  : parseInt(fields[0], 10),
+            height : parseFloat(fields[1])
+          };
 
-          var feature    = {};
-          feature.chr    = chrom;
-          feature.start  = parseInt(fields[0], 10);
-          feature.end    = feature.start + span;
-          feature.height = parseFloat(fields[1]);
+          feature.end = feature.start + span;
+
           features.push(feature);
         }
       }
