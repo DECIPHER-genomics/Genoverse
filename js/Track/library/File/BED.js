f18ede487fcff7c117ca9b15a34f86096c9b153c
Simon Brent <sb23@sanger.ac.uk>
2017-08-29 10:41:47 +0100

    Fixed popups for BED features so that they aren't full of undefined values

--- js/Track/library/File/BED.js
+++ js/Track/library/File/BED.js
@@ -1,26 +1,24 @@
 Genoverse.Track.File.BED = Genoverse.Track.File.extend({
-  name          : 'BED',
-  model         : Genoverse.Track.Model.File.BED,
-  bump          : true,
-  featureHeight : 10,
-  thickHeight   : 10,
-  thinHeight    : 7,
-  subFeatureJoinStyle : "curve",
+  name                : 'BED',
+  model               : Genoverse.Track.Model.File.BED,
+  bump                : true,
+  featureHeight       : 10,
+  thickHeight         : 10,
+  thinHeight          : 7,
+  subFeatureJoinStyle : 'curve',
+
   populateMenu: function (feature) {
-      return {
-        title       : '<a target=_blank href="https://genome.ucsc.edu/FAQ/FAQformat.html#format1">BED feature details</a>',
-        chrom       : feature.originalFeature[0],
-        chromStart  : feature.originalFeature[1],
-        chromEnd    : feature.originalFeature[2],
-        name        : feature.originalFeature[3],
-        score       : feature.originalFeature[4],
-        strand      : feature.originalFeature[5],
-        thickStart  : feature.originalFeature[6],
-        thickEnd    : feature.originalFeature[7],
-        itemRgb     : feature.originalFeature[8],
-        blockCount  : feature.originalFeature[9],
-        blockSizes  : feature.originalFeature[10],
-        blockStarts : feature.originalFeature[11]
-      };
+    var fields = [ false, false, false, 'name', 'score', 'strand', 'thickStart', 'thickEnd', 'itemRgb', 'blockCount', 'blockSizes', 'blockStarts' ]; // First three fields are chr, start, end which are covered by Location
+
+    return feature.originalFeature.reduce(function (menu, val, i) {
+      if (fields[i]) {
+        menu[fields[i]] = val;
+      }
+
+      return menu;
+    }, {
+      title    : '<a target="_blank" href="https://genome.ucsc.edu/FAQ/FAQformat.html#format1">BED feature details</a>',
+      Location : feature.chr + ':' + feature.start + '-' + feature.end
+    });
   }
 });
