3cca2f816c2603d7137ca7879895dee915b7d353
Tom Ratcliffe <tom.ratcliffe@congenica.com>
2021-04-26 17:53:31 +0100

    Add globals for test setup

--- test/require/0-setup.js
+++ test/require/0-setup.js
@@ -9,7 +9,15 @@ global.expect   = require('chai').expect;
 // Used for skipping some tests that fail on macOS due to very small differences in OS font rendering etc
 global.isMacOS = process.platform === 'darwin';
 
-// Genoverse library
-require(process.cwd() + '/index.js');
+global.jQuery = global.$ = require('jquery');
+
+global.Base = require('../../js/lib/Base.js');
+global.dallianceLib = require('../../js/lib/dalliance-lib.min.js');
+global.BWReader = require('../../js/lib/BWReader.js');
+global.VCFReader = require('../../js/lib/VCFReader.js');
+global.RTree = require('../../js/lib/rtree.js');
+global.jDataView = require('../../js/lib/jDataView.js');
+global.jParser = require('../../js/lib/jParser.js');
+global.Genoverse = require('../../index.js');
 
 Genoverse.ready.resolve();
